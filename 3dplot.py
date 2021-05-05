faa263f6810777cceeb8d4e9f1a6ea88aec6b4c9
Daniyar Enikeev <enikeev.dg@mipt.ru>
2021-05-05 23:49:08 +0300

    Пример 3д

---
+++ 3dplot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import sys
+
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+plt.show()
+plt.close()
+input("Press Enter to continue...")
