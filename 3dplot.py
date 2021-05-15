d9fad2397608537da5f425aca852acc14b2a1739
Daniyar Enikeev <enikeev.dg@mipt.ru>
2021-05-15 17:36:44 +0300

    Добавлен пример создания линии

--- 3dplot.py
+++ 3dplot.py
@@ -11,7 +11,7 @@ VecEnd_z  = 1
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot([VecStart_x, VecEnd_x], [VecStart_y,VecEnd_y],zs=[VecStart_z,VecEnd_z])
+ax.plot([VecStart_x, VecEnd_x], [VecStart_y,VecEnd_y],zs=[VecStart_z,VecEnd_z], marker='o')
 plt.show()
 plt.close()
 input("Press Enter to continue...")
