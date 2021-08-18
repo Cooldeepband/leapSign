d5ff49ec2f90968e935d735fd169098bb4d9d4bc
Daniyar Enikeev <enikeev.dg@mipt.ru>
2021-08-18 23:09:11 +0300

    Продолжена работа над записью всей инфы

--- leapPictures.py
+++ leapPictures.py
@@ -4,6 +4,9 @@ import cv2, Leap, math, ctypes
 import numpy as np
 import time
 from datetime import datetime, date
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
 
 def make_npimage(image):
     #wrap image data in numpy array
@@ -16,6 +19,9 @@ def make_npimage(image):
     img = np.reshape(as_numpy_array, (image.height, image.width))
     
     return img
+
+FINGER_TYPES = {0 : 'THUMB', 1 : 'INDEX', 2 : 'MIDDLE', 3 : 'RING', 4 : 'PINKY'}
+BONE_TYPES = {0 : 'TYPE_METACARPAL ', 1 : 'TYPE_PROXIMAL ', 2 : 'TYPE_INTERMEDIATE ', 3 : 'TYPE_DISTAL'}
 
 def run(controller):
     fileName = input('Введите имя файла: ')
@@ -34,26 +40,42 @@ def run(controller):
                 cv2.imwrite(imageDestL, image1)
                 cv2.imwrite(imageDestR, image2)
                 f = open(fileName + ' ' + fileDate + '.txt', 'w')
+                fig = plt.figure()
+                ax = fig.add_subplot(111, projection='3d')
                 for hand in frame.hands:
                     handType = "Left Hand" if hand.is_left else "Right hand"
+                    pltClr = 'blue' if hand.is_left else 'red'
+                    ax.plot(hand.palm_position.x, hand.palm_position.y, hand.palm_position.z, marker='o', color=pltClr)
                     f.write(handType + "Hand ID: " + str(hand.id) + "\n")
-                    f.write(" Palm Position: " \
-                        "x: " + str(hand.palm_position.x) + \
-                        "y: " + str(hand.palm_position.y) + \
-                        "z: " + str(hand.palm_position.z) + \
-                        "\n")
-                    f.write(" Palm Direction: " \
-                        "x: " + str(hand.direction.x) + \
-                        "y: " + str(hand.direction.y) + \
-                        "z: " + str(hand.direction.z) + \
-                        "\n")
+                    printXYZ(f, "Palm Position: ", hand.palm_position)
+                    printXYZ(f, "Palm Direction: ", hand.direction)
+
                     f.write("Pitch: " + str(hand.direction.pitch) + "\n")
                     f.write("Yaw: " + str(hand.direction.yaw) + "\n")
                     f.write("Roll: " + str(hand.direction.roll) + "\n")
                     f.write("\n")
+                    fingers = hand.fingers
+                    for finger in fingers:
+                        if finger.is_valid:
+                            f.write("Finger Type: " + FINGER_TYPES[finger.type] + "\n")
+                            for i in range(0, 4):
+                                bone = finger.bone(i)
+                                bone_start = bone.prev_joint
+                                bone_end = bone.next_joint
+
                 f.close()
+                plt.show()
+                if cv2.waitKey(10000) == ord('q'):
+                    plt.close()
                 cv2.destroyAllWindows()
                 break
+
+def printXYZ(file, descr, position):
+    file.write( descr + \
+    "x: " + str(position.x) + \
+    "y: " + str(position.y) + \
+    "z: " + str(position.z) + \
+    "\n")
 
 
 def main():
