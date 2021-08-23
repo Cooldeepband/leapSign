4f520b4692bb822f50f20a9d116ad749f780681d
Daniyar Enikeev <enikeev.dg@mipt.ru>
2021-08-23 23:21:06 +0300

    Картиночки

--- leapPictures.py
+++ leapPictures.py
@@ -35,8 +35,8 @@ def run(controller):
             cv2.imshow('Left Camera', image1)
             cv2.imshow('Right Camera', image2)
             if cv2.waitKey(1) == ord('a'):
-                imageDestL = 'C:/prgprojects/' + fileName + fileDate + '_L' + '.jpg'
-                imageDestR = 'C:/prgprojects/' + fileName + fileDate + '_R' + '.jpg'
+                imageDestL = 'C:/python_prog/' + fileName + fileDate + '_L' + '.jpg'
+                imageDestR = 'C:/python_prog/' + fileName + fileDate + '_R' + '.jpg'
                 cv2.imwrite(imageDestL, image1)
                 cv2.imwrite(imageDestR, image2)
                 f = open(fileName + ' ' + fileDate + '.txt', 'w')
@@ -45,7 +45,7 @@ def run(controller):
                 for hand in frame.hands:
                     handType = "Left Hand" if hand.is_left else "Right hand"
                     pltClr = 'blue' if hand.is_left else 'red'
-                    ax.plot(hand.palm_position.x, hand.palm_position.y, hand.palm_position.z, marker='o', color=pltClr)
+                    addDotToPlot(ax, hand.palm_position, pltClr)
                     f.write(handType + "Hand ID: " + str(hand.id) + "\n")
                     printXYZ(f, "Palm Position: ", hand.palm_position)
                     printXYZ(f, "Palm Direction: ", hand.direction)
@@ -55,27 +55,49 @@ def run(controller):
                     f.write("Roll: " + str(hand.direction.roll) + "\n")
                     f.write("\n")
                     fingers = hand.fingers
+                    fingersStart1 = []
+                    fingersStart2 = []
                     for finger in fingers:
                         if finger.is_valid:
                             f.write("Finger Type: " + FINGER_TYPES[finger.type] + "\n")
                             for i in range(0, 4):
                                 bone = finger.bone(i)
-                                bone_start = bone.prev_joint
-                                bone_end = bone.next_joint
+                                if i == 0:
+                                    fingersStart1.append(bone.prev_joint)
+                                elif i == 1 and finger.type != 0:
+                                    fingersStart2.append(bone.prev_joint)
+                                if bone.is_valid:
+                                    addLineToPlot(ax, bone.prev_joint, bone.next_joint, pltClr)
+                                    printXYZ(f, "Bone " + BONE_TYPES[i] + 'prev joint ', bone.prev_joint)
+                                    printXYZ(f, "Bone " + BONE_TYPES[i] + 'next joint ', bone.next_joint)
+                    if len(fingersStart1) == 5:
+                        for i in range(0, 4):
+                            addLineToPlot(ax, fingersStart1[i], fingersStart1[i + 1], 'blue')
+                    if len(fingersStart2) == 4:
+                        for i in range(0, 3):
+                            addLineToPlot(ax, fingersStart2[i], fingersStart2[i + 1], 'green')
 
                 f.close()
                 plt.show()
-                if cv2.waitKey(10000) == ord('q'):
-                    plt.close()
+                buttonPressed = False
+                while not(buttonPressed):
+                    buttonPressed = plt.waitforbuttonpress()
+                plt.close()
                 cv2.destroyAllWindows()
                 break
 
 def printXYZ(file, descr, position):
     file.write( descr + \
     "x: " + str(position.x) + \
-    "y: " + str(position.y) + \
-    "z: " + str(position.z) + \
+    " y: " + str(position.y) + \
+    " z: " + str(position.z) + \
     "\n")
+
+def addDotToPlot(figur, position, pltClr):
+    figur.plot(position.x, position.y, position.z, marker='o', color=pltClr)
+
+def addLineToPlot(figur, positionStart, positionEnd, pltClr):
+    figur.plot([positionStart.x, positionEnd.x], [positionStart.y,positionEnd.y],zs=[positionStart.z,positionEnd.z], marker='o', color=pltClr)
 
 
 def main():
